381651966d73827bf4fd3c4abda8f0e851ab5008
crabcatcher0 <rautprashant103@gmail.com>
2023-12-28 10:30:19 +0545

    remain

--- app_crm/urls.py
+++ app_crm/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name='home'),
+    path('logout/', views.logout_user, name='logout'),
 ]
--- app_crm/views.py
+++ app_crm/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html', {})
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "login in successful.")
+            return redirect('home')
+        else:
+            messages.success(request, "Username or Password Wrong.")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "Logged Out....")
+    return redirect ('home')
