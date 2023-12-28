acbb5793d7dd9dc66da1ad660e7fb9a7a295ad83
crabcatcher0 <rautprashant103@gmail.com>
2023-12-28 19:17:44 +0545

    still remain

--- app_crm/views.py
+++ app_crm/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
+
 
 # Create your views here.
 
@@ -25,3 +27,20 @@ def logout_user(request):
     logout(request)
     messages.success(request, "Logged Out....")
     return redirect ('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "Registered Successfuly")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form': form})
+    
+    return render(request, 'register.html', {'form': form})
